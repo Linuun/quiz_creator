0cc359bb7e1d221f2ef9562221ade5a9a872492f
Linuun <jlenonninolla31@gmail.com>
2025-04-08 16:16:34 +0800

    added pseudocode

---
+++ ask_user_to_input_questions.py
@@ -0,0 +1,7 @@
+# create a file using with and open function in append mode
+# use while loop
+# ask user to input a question, the possible answers, and the correct answer
+# print "Invalid Answer" if correct answer is not in choices
+# use write function to write the inputs of the user in the file
+# ask the user if they want to continue
+# break the loop if not
