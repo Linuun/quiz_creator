efa1a2bdd8b4aeec93a4e2492f4169244508b13d
Linuun <jlenonninolla31@gmail.com>
2025-04-08 16:31:51 +0800

    ask the user to input a question, the possible answers, and the correct answer

--- ask_user_to_input_questions.py
+++ ask_user_to_input_questions.py
@@ -3,7 +3,14 @@ with open("quiz.txt", "a") as file:
 # use while loop
     while True:
 # ask user to input a question, the possible answers, and the correct answer
-# print "Invalid Answer" if correct answer is not in choices
+        print("\n Let's build a fun quiz!")
+        question = input("Enter a question: ")
+        answer_a = input("A.) ")
+        answer_b = input("B.) ")
+        answer_c = input("C.) ")
+        answer_d = input("D.) ")
+        correct_answer = input("Enter the correct answer: ").upper()
+# print "Invalid Answer" if correct answer is not in choice
 # use write function to write the inputs of the user in the file
 # ask the user if they want to continue
 # break the loop if not
