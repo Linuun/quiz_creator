ac144bcfc24fe8f4700a214c8e1d8972ad270cdb
Linuun <jlenonninolla31@gmail.com>
2025-04-08 16:19:05 +0800

    use while loop

--- ask_user_to_input_questions.py
+++ ask_user_to_input_questions.py
@@ -1,6 +1,7 @@
 # create a file using with and open function in append mode
 with open("quiz.txt", "a") as file:
 # use while loop
+    while True:
 # ask user to input a question, the possible answers, and the correct answer
 # print "Invalid Answer" if correct answer is not in choices
 # use write function to write the inputs of the user in the file
