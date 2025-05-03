7ca83bc8bd301d16caef17cabfba8e61760eac61
Linuun <jlenonninolla31@gmail.com>
2025-05-03 22:41:14 +0800

    added pseudocode

---
+++ quiz_game.py
@@ -0,0 +1,20 @@
+# import libraries
+# ask the user to choose the quiz text file
+# use with and open function to open the file in read mode
+# split the contents of the file using a separator
+# extract the question, possible answers, and correct answer
+# convert answer letter to index
+# append (question, choices, correct_answer) to a list
+# return list of all valid questions
+# create a text box with a background and padding 
+# fill the background with some colors
+# initialize pygame and sets up the game
+# use while loop to run the game
+# handle events like quitting the game
+# checks answer and gives feedback
+# update the score and goes to the next question or ends the game
+# handles answer input
+# shows the question and choices in styled text boxes
+# shows the score and the instructions to exit the game
+# updates the display and limits to 30 frames/second.
+# run the entire program
